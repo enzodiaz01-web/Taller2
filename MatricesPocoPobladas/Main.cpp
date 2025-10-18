1e2d081be8d8893e94c74c23dd5b9ffeb9f2ac61
Enzo Diaz Parra <enzo.diaz01@alumnos.ucn.cl>
2025-10-18 02:25:03 -0300

    Modificacion del Main
    Se crearon y testaron tres nuevos metodos,"TestTime", DensityTime y "SparseMatrixRandom." A grandes rasgos, tenemos un metodo que rellena la matriz sin sobreescribirla , el otro calcul;a la densidad de las matrices y el otro, calcula el tiempo de incersion. Se espera crear un menu para mejorar la administracion de los metodos

--- MatricesPocoPobladas/Main.cpp
+++ MatricesPocoPobladas/Main.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
 #include "SparseMatrix.h"
+#include <set>
+#include <chrono>
 using namespace std;
 
+void SparseMatrixRandom(SparseMatrix &matrix, int Nelementos, int Xmax, int Ymax){
+    int cont =0;
+    while(cont<Nelementos){
+        int x = rand() % Xmax;
+        int y = rand() % Ymax;
+        if(matrix.get(x,y)==0){
+            int valor = rand() % 100 +1;
+            matrix.add(valor, x, y);
+            cont++;
+        }
+    }
+}
+void TestTime(int Nelementos, int Xmax, int Ymax){
+    SparseMatrix A;
+    auto start = chrono::high_resolution_clock::now();
+    SparseMatrixRandom(A, Nelementos, Xmax,Ymax);
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end - start;
+    cout <<"Se insertaron " << Nelementos <<" elementos, tiempo estimado " << elapsed.count() << "segundos" << endl;
+}
+
+
 int main() {
-    SparseMatrix A, B;
-
-    A.add(2, 0, 0);
-    A.add(6, 0, 1);
-
-    B.add(7, 0, 4);
-    B.add(2, 1, 4);
-
-    cout << "Matriz A:\n";
-    A.printStoredValues();
-
-    cout << "\nMatriz B:\n";
-    B.printStoredValues();
-
-    cout << "\nDensidad de A: ";
-    A.density();
-
-    SparseMatrix* C = A.multiply(&B);
-
-    cout << "\nResultado de A x B:\n";
-    C->printStoredValues();
-
-    delete C;
+    srand(time(0));
+    TestTime(50,100,100);
+    TestTime(250,200,200);
+    TestTime(500,300,300);
+    TestTime(1000,500,500);
+    TestTime(5000,1000,1000);
+    
     return 0;
 }
